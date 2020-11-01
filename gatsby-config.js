24403eb6cb6fd2b068ba490075205a41126269cf
yangqi02 <yangqi02@zuoyebang.com>
2020-11-01 22:17:05 +0800

    add: 图片、插件

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,22 @@
  */
 
 module.exports = {
+  siteMetadata: {
+    title: `My Gatsby`,
+    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    author: `@gatsbyjs`,
+  },
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`
+  ],
 }
